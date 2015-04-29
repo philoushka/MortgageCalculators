263806736fadc7d1d33045f959ca56e51e5205e5
Phil Campbell <philoushka@gmail.com>
2015-04-29 15:17:13 -0700

    Create SB.js

---
+++ InterestBuydown/SB.js
@@ -0,0 +1,14 @@
+var principalBalance = 250000;
+var Current SPR Rate = 5.8500;
+var buydownRate = 5.7500;
+var termMonths = 60;
+var termYears = termMonths/12;
+var amortizationMonths=	300;
+var amortizationYears=amortizationMonths/12;
+var daysPerMonth = 30.416666667;
+var constantExponent = 0.166666667;
+
+function calcEquivalentMonthlyPayment()
+{
+
+}
